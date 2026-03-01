822d3a44d7aaabc7984f3c7f5e82a265a96e1cec
webdvlpr <85422802+webdvlpr@users.noreply.github.com>
2026-03-01 10:28:27 +0530

    Changed url

--- backend/src/controllers/admin/auth.controller.js
+++ backend/src/controllers/admin/auth.controller.js
@@ -56,8 +56,8 @@ async function login(req, res) {
 
         res.cookie("admin_token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "lax",
+            secure: true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
