8756defe130155d7207afeb8cd9f28e6686d1c48
webdvlpr <85422802+webdvlpr@users.noreply.github.com>
2026-03-01 09:10:45 +0530

    changed server setting

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ const app = require("./app");
 // importing db config and running
 const connectDb = require("./config/db");
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Server started at port 3000");
 });
 connectDb();
